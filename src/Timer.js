d80b46f0a1c20d8dfd0b6e8afdcf1fe1c56b2d75
Ari E <48712583+AriEncarnacion@users.noreply.github.com>
2020-03-06 14:26:43 -0800

    Completed lab6
    All steps complete.

--- src/Timer.js
+++ src/Timer.js
@@ -1,15 +1,52 @@
 import * as React from "react";
 
 export function Timer(props) {
+  const [timer, setTimer] = React.useState(0);
+  const [isRunning, setIsRunning] = React.useState(false);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+  };
+
+  const renderStartStop = () => {
+    if (!isRunning) {
+      return <button onClick={startTimer}>Start</button>;
+    } else {
+      return <button onClick={stopTimer}>Stop</button>;
+    }
+  };
+
+  const resetTimer = () => {
+    setTimer(0);
+  };
+
+  React.useEffect(() => {
+    console.log("effect");
+    const interval = setInterval(() => {
+      // console.log("This will run every second!");
+      if (isRunning) {
+        setTimer(prev => {
+          return 32 + prev;
+        });
+      }
+    }, 32);
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
   return (
     <div>
-      <h1>0 {/* replace the 0 with the value in your timer state */}</h1>
+      <h1>{timer}</h1>
       <p>
         <small>Milliseconds elapsed</small>
       </p>
-      <button>Stop</button>
-      <button>Start</button>
-      <button>Reset</button>
+      {renderStartStop()}
+      {/* <button onClick={stopTimer}>Stop</button> */}
+      {/* <button onClick={startTimer}>Start</button> */}
+      <button onClick={resetTimer}>Reset</button>
     </div>
   );
 }
